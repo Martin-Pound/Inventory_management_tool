d9f8b4693932158c88c257e9a4deb911f6dd7af2
Martin-Pound <mpound001@gmail.com>
2025-11-30 15:06:30 +0000

    admins registered

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import (Category, Supplier, Warehouse, Bin, Item, StockLevel, MovementLog, MovementType)
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Supplier)
+admin.site.register(Warehouse)
+admin.site.register(Bin)
+admin.site.register(Item)
+admin.site.register(StockLevel)
+admin.site.register(MovementType)
+admin.site.register(MovementLog)
